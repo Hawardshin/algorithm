4cef57d00fa70a03aef164d8280bcc3001f5721f
haward shin(joushin) <92737157+Hawardshin@users.noreply.github.com>
2022-07-12 21:20:31 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,3 +1,4 @@
+//백준 2579
 #include<iostream>
 #include<algorithm>
 using namespace std;
@@ -16,5 +17,5 @@ int main()
     for(int y=4;y<=n;y++){
         dp[y]= max(dp[y-2]+a[y], dp[y-3]+a[y-1]+a[y]);
     }
-    cout << dp[n];
+    cout<< dp[n];
 }
