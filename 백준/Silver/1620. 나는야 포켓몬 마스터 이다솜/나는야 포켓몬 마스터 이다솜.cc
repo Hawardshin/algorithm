1c975b746a63201fbceff182479e0eea035fcef0
haward shin(joushin) <wnddms12345@naver.com>
2022-12-28 17:41:11 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 200 ms, Memory: 48988 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,87 +1,40 @@
-/*
-   포켓몬 도감에서 포켓몬의 이름을 보면 포켓몬의 번호를 말하거나, 포켓몬의 번호를 보면 포켓몬의 이름을 말하는 연습
-   입력 : 포켓몬의 개수 N ,맞춰야 하는 문제의 개수 M
-          1 ~ N 포켓몬 이름, 문제 : M개 숫자는 문자로 문자는 숫자로!
-   조건 :  1 <= N,M <= 100000,
-   */
-
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <map>
+#include <string>
 #include<sstream>
 using namespace std;
 
-typedef struct mmmm
-{
-        string name;
-        int number;
-}mons;
+ map <string, int> dic;
 
-bool sort_fuc(mons a,mons b)
-{
-        return (a.name < b.name);
-}
-
-bool is_num(const string& i)
-{
-        if ('0'<= i[0] && i[0] <='9')
-                return (true);
-        return (false);
-}
-
-int s_to_num(string s)
-{
-        istringstream ss(s);
-        int x;
-        ss >> x;
-        return (x);
-}
-
-void find_result(mons *monster,string target,int end)
-{
-        int start = 0;
-        int mid;
-        while (start <= end)
-        {
-                mid = (start + end) / 2;
-                if (monster[mid].name == target)
-                {
-                        cout << monster[mid].number << "\n";
-                        return;
-                }
-                else if (monster[mid].name > target)
-                {
-                        end = mid -1;
-                }
-                else
-                        start = mid + 1;
-        }
-}
-
-
+// int s_to_num(string s)
+// {
+//         istringstream ss(s);
+//         int x;
+//         ss >> x;
+//         return (x);
+// }
+string arr[1000000];
 int main()
 {
-    ios :: sync_with_stdio(0);
-    cin.tie(0);
-        int mon_n,matter_n;
-        cin >> mon_n >> matter_n;
-        mons *monster = new mons [mon_n];
-        mons *monster_name = new mons [mon_n];
-        for (int i = 0; i < mon_n; i++)
-        {
-                cin >> monster[i].name;
-                monster_name[i].name = monster[i].name;
-                monster[i].number = i + 1;
-                monster_name[i].number =i+1;
-        }
-        //cout << "\n-----------------------------------------------------------\n";
-        sort(monster,monster + mon_n,sort_fuc);
-        string input;
-        for (int i =0 ;i < matter_n; i++)
-        {
-                cin >> input;
-                if (is_num(input))
-                        cout << monster_name[s_to_num(input) - 1].name<<"\n";
-                else
-                        find_result(monster,input,mon_n -1);
-        }
+	ios :: sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int in_num;
+	int f_num;
+	string s;
+	cin >>  in_num >> f_num;
+	for (int i = 1; i <=in_num;i++)
+	{
+		cin >> s;
+		arr[i] = s;
+		dic.insert({s,i});
+	}
+	for (int i=0; i < f_num;i++)
+	{
+		cin >> s;
+		if ('0'<=s[0]&& s[0]<='9')
+			cout << arr[stoi(s)] << "\n";
+		else
+			cout << (int)(dic.find(s)->second) << "\n";
+	}
 }
