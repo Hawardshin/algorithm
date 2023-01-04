4d53cb9040a850bb6bdc2e4ffe2767cedc18b032
haward shin(joushin) <wnddms12345@naver.com>
2023-01-04 12:46:29 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -1,85 +1,61 @@
-#include<iostream>
-#include<algorithm>
-#include<string>
-#include<vector>
-#include<stack>
-#include<queue>
-#include<utility>
-
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-int arr[65][65];
-
-bool chk_all_same(int x,int y, int size)
+int check_all_same(int x,int y,int size,vector<vector<int>> &bmap)
 {
-	int t =arr[y][x];
-	for (int i=0;i < size;i++)
+	int start = bmap[y][x];
+	for (int i=y; i < y + size;i++)
 	{
-		for (int j=0; j<size;j++)
+		for (int j=x; j <x+ size;j++)
 		{
-			if (t != arr[i+y][j+x])
-				return(0);
+			if (bmap[i][j] != start)
+				return (0);
 		}
 	}
 	return (1);
 }
 
-// void	print_arr(int x, int y, int size)
-// {
-// 	for (int i=0; i <size;i++)
-// 	{
-// 		for (int j=0; j<size;j++)
-// 		{
-// 			cout << arr[y+i][x+j];
-// 		}
-// 	}
-// }
-
-
-void	recur(int x,int y, int size)
+void	recur(int x,int y,int size,vector<vector<int>> &bmap)
 {
-	if (size == 1)
+	if (size == 1|| check_all_same(x,y,size, bmap))
 	{
-		cout << arr[y][x];
-		return ;
-	}
-	if (chk_all_same(x,y,size))
-	{
-		cout << arr[y][x];
-		return ;
+		cout << bmap[y][x];
 	}
 	else
 	{
 		cout << "(";
-		for (int i=0; i<2;i++)
-		{
-			for (int j=0; j<2;j++)
-			{
-				recur(x+j*(size/2),y+i*(size/2),size/2);
-			}
-		}
-		cout <<")";
+		recur(x,y,size/2,bmap);
+		recur(x + size/2,y,size/2,bmap);
+		recur(x,y+ size/2,size/2,bmap);
+		recur(x+ size/2,y+ size/2,size/2,bmap);
+		cout << ")";
 	}
 }
 
 int main()
 {
+	int n;
+	cin >> n;
 
-	ios :: sync_with_stdio(0);
-	cin.tie(0);
-	int input;
-	cin >> input;
+	vector <vector<int>> bmap(n+1,vector<int>(n+1, 0));
 	string s;
-
-	for (int i=1; i <=input; i++)
+	for (int i=1;i <= n;i++)
 	{
-		cin >> s;
-		for (int j=1; j<=input;j++)
+		cin >>s;
+		for (int j=1;j <= n;j++)
 		{
-			arr[i][j] = s[j-1] - '0';
+			bmap[i][j] = (int)(s[j - 1]-'0');
 		}
 	}
-	// cout << "(";
-	recur(1,1,input);
-	// cout << ")";
+	// if (check_all_same(1,1,n,bmap) ==1)
+	// {
+	// 	cout << "(";
+	// 	cout << bmap[1][1];
+	// 	cout << ")";
+	// }
+	// else
+		recur(1,1,n,bmap);
 }
