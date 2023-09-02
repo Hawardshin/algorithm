ded77047cf761f48879cb137990e1ce65cc8d71f
haward shin(joushin) <wnddms12345@naver.com>
2023-09-03 01:02:40 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1072 ms, Memory: 29232 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+int input[1001];
+int links[1001][1001];
+int N,M;
+void dfs(int start){
+	for(int i=1;i <= N;i++){
+		if (links[i][start]){
+			links[i][start] = 0;
+			links[start][i] = 0;
+			dfs(i);
+			dfs(start);
+		}
+	}
+}
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> N >> M;
+	int a,b;
+	int ret = 0;
+	int cnt =0;
+	for(int i=0;i<M;i++){
+		cin >> a >>b;
+		links[a][b] = 1;
+		links[b][a] = 1;
+		if (!input[a])
+			cnt++;
+		if (!input[b])
+			cnt++;
+		input[a] = 1;
+		input[b] = 1;
+	}
+	for(int i=1;i <= N;i++){
+		int bef = i - 1;
+		for(int j=i + 1; j <= N;j++){
+			if (links[i][j]){
+				links[i][j] = 0;
+				links[j][i] = 0;
+				dfs(i);
+				dfs(j);
+				if (bef != i){
+					bef = i;
+					ret++;
+				}
+			}
+		}
+	}
+	ret += (N - cnt);
+	cout << ret<<"\n";
+}
