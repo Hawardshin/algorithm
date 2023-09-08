ee727bf7ab6c202e09c4de3cc68758a44e320867
haward shin(joushin) <wnddms12345@naver.com>
2023-09-08 15:42:35 +0900

    [Gold V] Title: 치킨 배달, Time: 372 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,46 +1,39 @@
+#include <vector>
 #include <iostream>
 #include <algorithm>
-#include <queue>
-int mapp[51][51];//지도
-int	vis[51][51];//bfs
+#include <utility>
+#include <cstring>
+using namespace std;
+int table[51][51];//지도
 int valid[51][51];//치킨집 사용가능여부
 int n,m;
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
-// int chi_num;
-using namespace std;
-//
+vector<int> idx_ar;
+vector<pair<int,int> >chk_ar;
+int chk_size;
+
 int min_point=21474836;
 
-int	find_min(int y,int x)
+int	find_min(int y,int x)//y,x위치에서 거리인 치킨집을 찾는다.
 {
 	int len = 0;
 	int min = 21474836;
-	for (int i=1;i <= n; i++)
-	{
-		for (int j=1;j <=n; j++)
-		{
-			if (valid[i][j] == 1)
-			{
-				// cout<<i << " "<< j<< "\n";
+	for (int i=1;i <= n; i++){
+		for (int j=1;j <=n; j++){
+			if (valid[i][j] == 1){
 				len = abs(x - j) + abs(y - i);
 				if(len < min)
 					min = len;
 			}
 		}
 	}
-	// cout<< min << "----\n";
 	return (min);
 }
 
-void	cal_point()
-{
+void	cal_point(){//최소 점수 저장
 	int	min =0;
-	for (int i=1;i<=n;i++)
-	{
-		for(int j=1;j<=n;j++)
-		{
-			if (mapp[i][j] == 1)
+	for (int i=1;i<=n;i++){
+		for(int j=1;j<=n;j++){
+			if (table[i][j] == 1)
 				min += find_min(i,j);
 		}
 	}
@@ -48,47 +41,33 @@ void	cal_point()
 		min_point = min;
 }
 
-void	valid_recur(int depth, int i,int j)
+int main()
 {
-	if (depth == m)
-		cal_point();
-	if (i == n + 1)
-		return ;
-	for (int ti=i;ti <=n;ti++)
-	{
-		for (int tj=0;tj<=n;tj++)
-		{
-			if (i == ti && tj == 0)
-				tj = j;
-			if (mapp[ti][tj] == 2)
-			{
-				valid[ti][tj] = 1;
-				if (tj == n)
-					valid_recur(depth+1,ti+1,0);
-				else
-					valid_recur(depth+1,ti,tj + 1);
-				valid[ti][tj] = 0;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n>>m;
+
+	for (int i=1;i <=n;i++){
+		for (int j=1;j <=n;j++){
+			cin >> table[i][j];
+			if (table[i][j] == 2){
+				chk_ar.push_back({i,j});
+				chk_size++;
 			}
 		}
 	}
-}
-int main()
-{
-	cin >> n>>m;
-
-	for (int i=1;i <=n;i++)
-	{
-		for (int j=1;j <=n;j++)
-		{
-			cin >> mapp[i][j];
+	for(int i=0;i < chk_size - m;i++) idx_ar.push_back(0);
+	for(int i=0;i< m;i++)idx_ar.push_back(1);
+	do{
+		for(int i=0;i < chk_size;i++){
+			if (idx_ar[i] == 1){
+				int y = chk_ar[i].first;
+				int x = chk_ar[i].second;
+				valid[y][x] = true;
+			}
 		}
-	}
-	// for (int i=0; i <=n;i++)
-	// {
-	// 	for (int j=0;j<=n;j++) cout<< mapp[i][j] << " ";
-	// 	cout<<endl;
-	// }
-	// cal_point();
-	valid_recur(0,0,0);
-	cout<< min_point;
+		cal_point();
+		memset(valid, 0,sizeof(valid));
+	}while (next_permutation(idx_ar.begin(), idx_ar.end()));
+	cout<< min_point << "\n";
 }
