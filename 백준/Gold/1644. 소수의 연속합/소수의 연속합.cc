b9ad8ecd1adf2e0370f52815c449e3cad71dbd8c
haward shin(joushin) <wnddms12345@naver.com>
2023-09-14 01:17:44 +0900

    [Gold III] Title: 소수의 연속합, Time: 20 ms, Memory: 40260 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+bool sosu[4000001];
+vector<int> sosu_ar;
+long long sum_ar[4000001];
+int ret =0;
+void Eratos(int n){//구글 위키백과 참고
+	if (n <= 1)
+		return ;
+	for(long long i=2;i <=n;i++){
+		if (!sosu[i]){
+			//i*k (k < i) 까지는 이미 검사되었으므로 j시작 값은 i*2에서 i*i로 개선할 수 있다.
+			sosu_ar.push_back(i);
+			for(long long j=i*i; j <=n;j+=i){
+				sosu[j] = true;
+			}
+		}
+	}
+}
+
+int main(){
+	int N;
+	cin >> N;
+	Eratos(N);
+	int sosu_num = sosu_ar.size();
+	// for(auto it : sosu_ar) cout << it << " ";
+	// cout << "\n";
+  int s = 0, e = 0,sum=0;
+	if (sosu_num == 0)
+	{
+		cout << 0 << '\n';
+		return (0);
+	}
+	while (e <= sosu_num) {
+		if (sum < N) {
+			sum += sosu_ar[e];
+			e++;
+			continue;
+		}
+		if (sum == N) 
+			ret++;
+		sum -= sosu_ar[s];
+		s++;
+  }
+	cout << ret<< "\n";
+}
