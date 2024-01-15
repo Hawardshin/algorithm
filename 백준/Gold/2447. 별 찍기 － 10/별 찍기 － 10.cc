d4e9e2b1f10f78656751cdcffad0d9b186b0acee
haward shin(joushin) <wnddms12345@naver.com>
2024-01-15 18:28:21 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 276 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+int N;
+
+void make_star(int i,int j,int size){
+	if (((i / size) %3 == 1 && (j / size) %3 == 1) )
+		cout << " ";
+	else {
+		if (size == 1)
+			cout << "*";
+		else 
+			make_star(i,j, size/3);
+	}
+}
+
+int main(){
+	cin >> N;
+	for(int i=0;i<N;i++){
+		for(int j=0;j<N;j++){
+			make_star(i,j,N);
+		}
+		cout << "\n";
+	}
+
+}
