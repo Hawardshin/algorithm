f6d71915a65963f617719f21bed8dd22f19e7bf2
haward shin(joushin) <wnddms12345@naver.com>
2024-02-19 14:52:11 +0900

    [Silver IV] Title: 암기왕, Time: 600 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.cc
+++ 백준/Silver/2776. 암기왕/암기왕.cc
@@ -8,18 +8,6 @@ using namespace std;
 //mid = 1 
 int T,N,M;
 int note[1000000];
-bool binarySearch(int st,int ed, int toFind){
-	int mid = (st+ed)/2;
-	if (note[mid] == toFind)
-		return true;
-	if (st > ed)
-		return false;
-	if (note[mid] > toFind)
-		return binarySearch(st,mid-1,toFind);
-	else 
-		return binarySearch(mid+1,ed,toFind);
-
-}
 
 int main(){
 	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
@@ -32,7 +20,11 @@ int main(){
 		for(int i=0;i<M;i++) {
 			int tmp;
 			cin >> tmp;
-			cout << binarySearch(0,N-1,tmp) << "\n";
+			auto a = (lower_bound(note,note+N,tmp));
+			if (a >= note+N)
+				cout << 0 <<"\n";
+			else 
+				cout << (*a == tmp) << "\n";
 		}
 		T--;
 	}
