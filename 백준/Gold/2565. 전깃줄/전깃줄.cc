e0fd7ab93b1c9bd7ae1a1b03d1335dfaba5a9260
haward shin(joushin) <wnddms12345@naver.com>
2024-02-20 14:32:59 +0900

    [Gold V] Title: 전깃줄, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.cc
+++ 백준/Gold/2565. 전깃줄/전깃줄.cc
@@ -1,36 +1,35 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#define X first
+#define Y second
 using namespace std;
 
+pair<int,int> bolt[101];
+int dp[101];
+int n;
+int maxRet;
 
-int N;
-vector<pair<int,int>> input;
-int ret= 0;
-int DP[101];
-int max_size=0;
 int calcDp(int i){
-	int now_num = input[i].second;
-	int calc_ret=1;
-	for(int j=0; j <i;j++){
-		if (input[j].second < now_num)
-			calc_ret = max(DP[j]+1,calc_ret);
+	int ret = 1;
+	for(int j=i-1; j >=0;j--){
+		if (bolt[j].Y < bolt[i].Y){
+			ret = max(dp[j]+1,ret);
+			maxRet = max(ret,maxRet);
+		}
 	}
-	max_size = max(max_size, calc_ret);
-	return calc_ret;
+	return ret;
 }
 
-int main(){
-	cin >> N;
-	int a,b;
 
-	for(int i=0;i<N;i++){
-		cin >> a >> b;
-		input.push_back({a,b});
+int main(){
+	cin >> n;
+	for(int i=0;i<n;i++){
+		cin>> bolt[i].X >> bolt[i].Y;
 	}
-	DP[0] = 1;
-	sort(input.begin(),input.end());
-	for(int i=1;i<N;i++) DP[i] = calcDp(i);
-	
-	cout << N-max_size << "\n";
+	sort(bolt,bolt+n);
+	for(int i=0;i<n;i++){
+		dp[i] = calcDp(i);
+		// cout << dp[i] <<"\n";
+	}
+	cout << n- maxRet <<"\n";
 }
