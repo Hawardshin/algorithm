c5ebe1291b349b9ac60db5133b0e961560bca6ff
haward shin(joushin) <wnddms12345@naver.com>
2024-02-20 13:15:12 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -1,49 +1,54 @@
-
 #include <iostream>
 #include <algorithm>
 #include <vector>
 using namespace std;
-int arr[1001];
-int DP[1001];
-vector<int> ret2[1001];
+//앞에 해당 숫자보다 작은 숫자가 있다면?
+//해당숫자로 시작하는 최대값은 있을 수 없다.
+
+
+
 int N;
-int max_s = 1;
-int max_idx;
-int calcDp(int i){
-	int ret =1;
-	for(int j=0;j<i;j++){
-		if (arr[j] < arr[i]){
-			if (ret < DP[j] +1){
-				ret = DP[j] + 1;
-				ret2[i] = ret2[j];
-				ret2[i].push_back(arr[i]);
+int arr[1001];
+int dist[1001];
+int ar[1001];
+int maxLen=1;
+int main(){
+	cin >> N;
+	for(int i=0;i<N;i++){
+		cin >> arr[i];
+	}
+	for(int i=N-1;i>=0;i--){
+		dist[i] = 1;
+		// cout << i <<"\n";
+		for(int j=i+1;j<N;j++){
+			if (arr[i] < arr[j]){
+				// cout << "xx:"<< j <<"\n";
+				if (dist[i] < dist[j]+1){
+					// cout << j << "가 업데이트"<<"\n";
+					ar[i] = j;
+					dist[i] =  dist[j] + 1;
+					maxLen = max(maxLen,dist[i]);
+				}
 			}
 		}
 	}
-	if (max_s < ret){
-		max_s = ret;
-		max_idx = i;
+	cout << maxLen<<"\n";
+	if (maxLen == 1){
+		cout << arr[0];
+		return 0;
 	}
-	if (ret2[i].size() == 0)
-		ret2[i].push_back(arr[i]);
-	max_s = max(ret, max_s);
-	return ret;
-}
-
-int main(){
-	cin >> N;
-	for(int i=0;i<N;i++) cin >> arr[i];
-	DP[0] = 1;
-	ret2[0].push_back(arr[0]);
-	for(int i=1;i<N;i++) DP[i] = calcDp(i);
-	cout << max_s << "\n";
-	for(auto a: ret2[max_idx]){
-		cout << a << " ";
+	for(int i=0;i<N;i++){
+		if (dist[i] == maxLen){
+			int n = i;
+			while (1){
+				cout << arr[n] << " ";
+				n = ar[n];
+				if (ar[n] ==0){
+					cout << arr[n] << " ";
+					return (0);
+				}
+			}
+		}
 	}
-	cout << "\n";
-}
 
-/*
-4
-2 1 2 5
-*/
+}
