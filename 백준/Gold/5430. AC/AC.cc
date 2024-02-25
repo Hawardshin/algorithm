8eafb48cda0617acbd3bbe9db3eea58ee9e6a2a0
haward shin(joushin) <wnddms12345@naver.com>
2024-02-25 20:15:02 +0900

    [Gold V] Title: AC, Time: 36 ms, Memory: 3112 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.cc
+++ 백준/Gold/5430. AC/AC.cc
@@ -1,118 +1,68 @@
-#include<iostream>
-#include<string>
+#include <deque>
+#include <iostream>
+
 using namespace std;
 
-int arr[500000];
-int head, tail;
+int n;
 
-int stoi(int i, int k, string& s)
-{
-        int result = 0;
-        for (int j= i; j <i+k; j++)
-        {
-                result = result *10 + (s[j] -'0');
-        }
-        //cout << result<< "result\n";
-        return (result);
-}
+string command;
+int arSize;
+string numAr;
+int main(){
+	ios::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+	cin >> n;
+	for(int i=0;i<n;i++){
+		deque<int>dq;
+		cin >> command >> arSize>>numAr;
+		int num = 0;
+		for(int j=0;j<numAr.size();j++){
+			if ('0' <=numAr[j] &&numAr[j]<='9'){
+				num*=10;
+				num+=(numAr[j] -'0');
+			}else if (arSize != 0 && num !=0){
+				// cout << "::"<<num <<"\n";
+				dq.push_back(num);
+				arSize--;
+				num = 0;
+			}
+		}
+		bool isReverse = false;
+		bool isError = false;
+		for(int j=0;j<command.size();j++){
+			if (command[j] == 'R')
+					isReverse = !isReverse;
+			else if (command[j] == 'D'){
+				if (dq.empty()){
+					isError=true;
+					break;
+				}
+				if (isReverse == true){
+					dq.pop_back();
+				}else {
+					dq.pop_front();
+				}
+			}
+		}
+		if (isError)
+			cout << "error\n";
+		else 
+		{
+			cout << "[";
+			while (!dq.empty()){
+				if (isReverse){
+					cout << dq.back();
+					dq.pop_back();
+				}
+				else{
+					cout << dq.front();
+					dq.pop_front();
+				}
+				if (!dq.empty()){
+					cout << ",";
+				}
+			}	
+			cout << "]\n";
+		}
+	}
 
-void stoar(string& s)
-{
-        int s_size = s.size();
-
-        int ari = 100000;
-        for (int i =0;  i< s_size; i++)
-        {
-                int k = 0;
-                while (s[i+k] >='0' && s[i+k] <='9')
-                {
-                        k++;
-                }
-                if (k!=0)
-                {
-                        arr[ari] = stoi(i,k,s);
-                        i+=k -1;
-                        ari++;
-                }
-        }
-}
-int main()
-{
-        cin.tie(0);
-        ios::sync_with_stdio(0);
-        int tcase_n;
-        int error= 0;
-        string command,arr_in;
-        int arr_n,c_size;
-        cin >> tcase_n;
-        for (int i= 0;  i<tcase_n; i++)
-        {
-                int error= 0;
-                cin >> command >> arr_n>> arr_in;
-                head = 100000;
-                tail = 100000+arr_n;
-                if (arr_n != 0)
-                        stoar(arr_in);
-                c_size = command.size();
-                for (int j = 0; j < c_size; j++)
-                {
-                        if (command[j] == 'R')
-                        {
-                                int tmp;
-                                if (head == tail)
-                                {
-                                }
-                                else if (head < tail)
-                                {
-                                        tmp = head;
-                                        head = tail - 1;
-                                        tail = tmp  - 1;
-                                }
-                                else
-                                {
-                                        tmp = head;
-                                        head = tail + 1;
-                                        tail = tmp + 1;
-                                }
-                        }
-                        else//command ==D
-                        {
-                                if (head == tail)
-                                {
-                                        error = 1;
-                                        cout << "error\n";
-                                        break;
-                                }
-                                else if (head > tail)
-                                        head--;
-                                else//head < tail
-                                        head++;
-                        }
-                }
-                if (error==1)
-                {
-                }
-                else if (head < tail)
-                {
-                        cout << '[';
-                        for (int j = head; j < tail; j++)
-                        {
-                                cout << arr[j];
-                                if (j != tail-1)
-                                        cout <<',';
-                        }
-                        cout << "]\n";
-                }
-                else
-                {
-                        cout << '[';
-                        for (int j = head; j > tail; j--)
-                        {
-                                cout << arr[j];
-                                if (j != tail + 1)
-                                        cout <<',';
-                        }
-                        cout << "]\n";
-                }
-        }
 }
