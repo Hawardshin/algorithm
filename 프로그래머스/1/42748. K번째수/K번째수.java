914c47dc47057c88bdc70895277d7e89450e6aae
hawardShin <wnddms12345@naver.com>
2025-04-06 09:41:19 +0900

    [level 1] Title: K번째수, Time: 0.38 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int [] ans = new int[commands.length];
+        // Vector<Integer> vec = new Vector<Integer>();
+        for(int i=0;i<commands.length;i++){
+            int[] ar= Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
+            Arrays.sort(ar);
+//             for(int a : ar){
+//                 System.out.print(a);
+//             }
+            
+            // System.out.println( "|"+ (commands[i][2] -1) + "|");
+            ans[i] = ar[commands[i][2] -1];
+            // vec.add(ar[commands[i][2] -1]);
+        }
+        // int [] ans = vec.toArray(int[].class);
+        return ans;
+        
+    }
+}
