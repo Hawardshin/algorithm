a5a914d99d25250094b8524fa53873e67b68f0e7
hawardShin <wnddms12345@naver.com>
2025-04-11 08:39:40 +0900

    [Silver II] Title: 스택 수열, Time: 48 ms, Memory: 2484 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -1,43 +1,36 @@
 #include<iostream>
 #include<stack>
 using namespace std;
-char result[10000000];
-int main()
-{
-        stack <int> stack;
-        int input_num;
-        cin >> input_num;
-        int *sequence = new int [input_num];
 
-        for (int i = 0; i < input_num; i++)
-        {
-                cin >> sequence[i];
+int main(){
+    int n; cin >>n;
+    stack <int> st;
+    int input[100001];
+    for(int i=0;i<n;i++) cin >> input[i];
+    int i_idx = 0;
+    bool answer[200001];
+    int num = 1;
+    int a_idx = 0;
+    while(i_idx < n && a_idx < 2*n){
+        if (st.empty() || num != st.top()){
+            st.push(num);
+            answer[a_idx++]= true;
+            num++;
         }
-        int i = 0;
-        int rindex = 0;
-        for (int j = 1; j <= input_num; j++)
-        {
-                stack.push(j);
-                result[rindex] ='+';
-                rindex++;
-                while(!stack.empty() &&sequence[i] == stack.top())
-                {
-                        stack.pop();
-                        result[rindex] = '-';
-                        rindex++;
-                        i++;
-                }
+        while (!st.empty() && st.top() == input[i_idx]){
+            st.pop();
+            answer[a_idx++] = false;
+            i_idx++;
         }
-        if (!stack.empty())
-        {
-                cout << "NO\n";
+    }
+    if (!st.empty() || i_idx != n)
+        cout << "NO\n";
+    else{
+        for(int i=0;i<a_idx;i++){
+            if (answer[i] == true)
+                cout << "+\n";
+            else
+                cout << "-\n";
         }
-        else
-        {
-                for (int k = 0; k < rindex;k++)
-                {
-                        cout << result[k]<< "\n";
-                }
-        }
-        return (0);
+    }
 }
