0f732d105ad689f9f0f820dc53b285ba45592172
hawardShin <wnddms12345@naver.com>
2025-04-24 18:57:16 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.cc
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -1,26 +1,17 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
-int twos[] ={1,2,4,8,16,32,64};
-
 int main(){
-	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-	int N; cin >> N;
-
-	for(int i=0;i <= (1<<6);i++){
-		int sum = 0;
-		int ret = 0;
-		for(int j=0;j < 7;j++){
-			if (i & (1 << j)){
-				sum+=twos[j];
-				ret++;
-			}
-		}
-		if (sum == N)
-		{
-			cout<< ret << "\n";
-			return 0;
-		}
-	}
+    int x;cin>>x;
+    int n = 1;
+    int ret =0;
+    while (n<=64){
+        if (n & x){
+            ret++;
+            x = x -n;
+        }
+        n*=2;
+    }
+    cout << ret<<"\n";
 }
